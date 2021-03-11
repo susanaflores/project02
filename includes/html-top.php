83789671434bceac69e91ba2b071d21e0c49e0d4
gromanaz <gromanaz@users.noreply.github.com>
2021-03-11 18:28:43 +0100

    revised html top + created php include for that

---
+++ includes/html-top.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>DMS 290 - Project 01 - Team San Francisco</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
+</head>
