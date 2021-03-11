0b72f262743b9a7fb2cd1b8ecf414f388aa59398
gromanaz <gromanaz@users.noreply.github.com>
2021-03-11 19:05:27 +0100

    + nav include

---
+++ includes/nav.php
@@ -0,0 +1,17 @@
+<!-- Links (sit on top) -->
+<nav class="w3-top">
+  <ul class="w3-row w3-padding w3-black">
+    <li class="w3-col s3">
+      <a href="san_francisco.html" class="w3-button w3-block w3-black">SAN FRANCISCO</a>
+    </li>
+    <li class="w3-col s3">
+      <a href="giza.html" class="w3-button w3-block w3-black">GIZA</a>
+    </li>
+    <li class="w3-col s3">
+      <a href="fuzhou.html" class="w3-button w3-block w3-black">FUZHOU</a>
+    </li>
+    <li class="w3-col s3">
+      <a href="yangzhou.html" class="w3-button w3-block w3-black">YANGZHOU</a>
+    </li>
+  </ul>
+</nav>
