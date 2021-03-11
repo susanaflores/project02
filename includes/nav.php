75b80bbb6ee3b3a960a0ad17174573cf06d4a1aa
gromanaz <gromanaz@users.noreply.github.com>
2021-03-12 00:39:27 +0100

    current menu highlighter

--- includes/nav.php
+++ includes/nav.php
@@ -1,17 +1,9 @@
 <!-- Links (sit on top) -->
-<nav class="w3-top navigation">
+<nav class="navigation w3-top">
   <ul class="w3-row w3-padding w3-black">
-    <li class="w3-col s3">
-      <a href="san_francisco.php" class="w3-button w3-block w3-black">San Francisco</a>
-    </li>
-    <li class="w3-col s3">
-      <a href="giza.php" class="w3-button w3-block w3-black">Giza</a>
-    </li>
-    <li class="w3-col s3">
-      <a href="fuzhou.php" class="w3-button w3-block w3-black">Fuzhou</a>
-    </li>
-    <li class="w3-col s3">
-      <a href="yangzhou.php" class="w3-button w3-block w3-black">Yangzhou</a>
-    </li>
+    <li class="w3-col s3"><a href="san_francisco.php" class="w3-button w3-block w3-black">San Francisco</a></li>
+    <li class="w3-col s3"><a href="giza.php" class="w3-button w3-block w3-black">Giza</a></li>
+    <li class="w3-col s3"><a href="fuzhou.php" class="w3-button w3-block w3-black">Fuzhou</a></li>
+    <li class="w3-col s3"><a href="yangzhou.php" class="w3-button w3-block w3-black">Yangzhou</a></li>
   </ul>
 </nav>
