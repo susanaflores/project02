9e626b0b2ca3b6db6f9a13b7f22593ac64b5f09c
Jeffron <Jeffron>
2021-04-06 23:29:29 -0400

    Changed layout of san francisco page

--- includes/html-top.php
+++ includes/html-top.php
@@ -2,10 +2,10 @@
 <html lang="en">
 
 <head>
-  <title>DMS 290 - Project 01 - Team San Francisco</title>
+  <title>DMS 290 - Project 02 - Team San Francisco</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inconsolata">
   <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/nav.css">
+ 
 </head>
--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,8 @@
-<nav class="navigation w3-top">
-  <ul class="w3-row w3-padding w3-black">
-    <li class="w3-col s3"><a href="san_francisco.php" class="w3-button w3-block w3-black">San Francisco</a></li>
-    <li class="w3-col s3"><a href="giza.php" class="w3-button w3-block w3-black">Giza</a></li>
-    <li class="w3-col s3"><a href="fuzhou.php" class="w3-button w3-block w3-black">Fuzhou</a></li>
-    <li class="w3-col s3"><a href="yangzhou.php" class="w3-button w3-block w3-black">Yangzhou</a></li>
-  </ul>
-</nav>
+<nav>
+    <ul>
+      <li><a href="san_francisco.php">San Francisco</a></li>
+      <li><a href="giza.php">Giza</a></li>
+      <li><a href="fuzhou.php">Fuzhou</a></li>
+      <li><a href="yangzhou.php">Yangzhou</a></li>
+    </ul>
+  </nav>
