4e76ffa796a9276ea3e68017fafcf552a8d36899
Jeffron <Jeffron>
2021-04-06 23:57:46 -0400

    Changed layout for Giza Page

--- san_francisco.php
+++ san_francisco.php
@@ -1,139 +1,140 @@
 <?php include "includes/html-top.php"; ?>
 
 <body>
-<header>
-  <div>
-    <a href="index.html">
-      <img src="assets/logo.jpg" alt="logo">
-    </a>
-  </div>
-  <?php include "includes/nav.php";?>
-</header>
+  <header>
+    <div>
+      <a href="index.html">
+        <img src="assets/logo.jpg" alt="logo">
+      </a>
+    </div>
+    <?php include "includes/nav.php";?>
+  </header>
 
 
-<div>
-  <div>
-    <img src="assets/sanfrancisco.jpg" alt="city photo">
-  </div>
-    <h2>Introduction</h2>
-    <p>
-      San Francisco, officially the City and County of San Francisco, is a cultural, commercial, and financial center of Northern California. San Francisco is the 16th most populous city in the United States, and the fourth most populous in California, with 881,549 residents as of 2019.
-    </p>
-    <p>
-      It covers an area of about 46.89 square miles (121.4 km2), mostly at the north end of the San Francisco Peninsula in the San Francisco Bay Area, making it the second most densely populated large U.S. city, and the fifth most densely populated U.S. county, behind only four of the five New York City boroughs.
-    </p>
-
-    <p>
-      San Francisco is part of the 12th-largest metropolitan statistical area in the United States by population, with 4.7 million people, and the fourth-largest by economic output, with GDP of $592 billion in 2019. With San Jose, it forms the fifth most populous combined statistical area in the United States, with 9.67 million residents as of 2019. Colloquial nicknames for San Francisco include The City, SF, Frisco and San Fran.
-    </p>
-  </div>
-  <div>
-    <a href="http://jaddy.digitalscholar.rochester.edu/dms290/assignment1/index.html">Learn more about San Francisco</a>
-  </div>
-</div>
-
-<div>
-  <div>
-    <img src="assets/san_francisco_geo.jpg" alt="San Francisco Geography">
-  </div>
-  <div>
-    <h2>Geography & Demographics</h2>
-    <p>
-      San Francisco is located on the West Coast of the United States at the north end of the San Francisco Peninsula and includes significant stretches of the Pacific Ocean and San Francisco Bay within its boundaries. Several picturesque islands - Alcatraz, Treasure Island and the adjacent Yerba Buena Island, and small portions of Alameda Island, Red Rock Island, and Angel Island - are part of the city. Also included are the uninhabited Farallon Islands, 27 miles (43 km) offshore in the Pacific Ocean. The mainland within the city limits roughly forms a "seven-by-seven-mile square", a common local colloquialism referring to the city's shape, though its total area, including water, is nearly 232 square miles (600 km2).
-    </p>
-
-    <p>
-      With a 2020 population of 883,255, it is the 4th largest city in California and the 17th largest city in the United States. San Francisco is currently growing at a rate of 0.10% annually and its population has increased by 9.69% since the most recent census, which recorded a population of 805,235 in 2010. San Francisco reached it's highest population of 883,255 in 2021. Spanning over 232 miles, San Francisco has a population density of 18,832 people per square mile.
-    </p>
-  </div>
-  <div>
-    <a href="http://jaddy.digitalscholar.rochester.edu/dms290/assignment1/index.html">Learn more about San Francisco</a>
-  </div>
-</div>
-
-<div>
-  <div>
-    <img src="assets/san_francisco_fog.jpg">
-  </div>
-  <div>
-    <h2>Climate</h2>
-       <p>
-        San Francisco has a warm-summer Mediterranean climate characteristic of California's coast, with moist mild winters and dry summers. San Francisco's weather is strongly influenced by the cool currents of the Pacific Ocean on the west side of the city, and the water of San Francisco Bay to the north and east. This moderates temperature swings and produces a remarkably mild year-round climate with little seasonal temperature variation.
+  <div class="info">
+    <div>
+      <img src="assets/sanfrancisco.jpg" alt="city photo">
+    </div>
+    <div class="para">
+      <h2>Introduction</h2>
+      <p>
+        San Francisco, officially the City and County of San Francisco, is a cultural, commercial, and financial center of Northern California. San Francisco is the 16th most populous city in the United States, and the fourth most populous in California, with 881,549 residents as of 2019.
+      </p>
+      <p>
+        It covers an area of about 46.89 square miles (121.4 km2), mostly at the north end of the San Francisco Peninsula in the San Francisco Bay Area, making it the second most densely populated large U.S. city, and the fifth most densely populated U.S. county, behind only four of the five New York City boroughs.
       </p>
 
       <p>
-        Among major U.S. cities, San Francisco has the coolest daily mean, maximum, and minimum temperatures for June, July, and August. During the summer, rising hot air in California's interior valleys creates a low pressure area that draws winds from the North Pacific High through the Golden Gate, which creates the city's characteristic cool winds and fog. The fog is less pronounced in eastern neighborhoods and during the late summer and early fall. As a result, the year's warmest month, on average, is September, and on average, October is warmer than July, especially in daytime.
+        San Francisco is part of the 12th-largest metropolitan statistical area in the United States by population, with 4.7 million people, and the fourth-largest by economic output, with GDP of $592 billion in 2019. With San Jose, it forms the fifth most populous combined statistical area in the United States, with 9.67 million residents as of 2019. Colloquial nicknames for San Francisco include The City, SF, Frisco and San Fran.
       </p>
-  </div>
-</div>
-
-<div>
-  <div>
-    <img src="assets/san_francisco_1846.jpg" alt="San Francisco - History">
-  </div>
-  <div>
-    <h2>History</h2>
-    <p>
-        The earliest archaeological evidence of human habitation of the territory of the city of San Francisco dates to 3000 BC. The Yelamu group of the Ohlone people resided in a few small villages when an overland Spanish exploration party, led by Don Gaspar de Portolá, arrived on November 2, 1769, the first documented European visit to San Francisco Bay.
-      </p>
-      <p>
-        The first maritime presence occurred on August 5, 1775, when San Carlos - commanded by Juan Manuel de Ayala - became the first ship to anchor in the bay. The following year, on March 28, 1776, the Spanish established the Presidio of San Francisco, followed by a mission, Mission San Francisco de Así­s (Mission Dolores), established by the Spanish explorer Juan Bautista de Anza.
-      </p>
-  </div>
-  <div class="more">
+    </div>
+    <div>
       <a href="http://jaddy.digitalscholar.rochester.edu/dms290/assignment1/index.html">Learn more about San Francisco</a>
+    </div>
   </div>
-</div>
-    
-<div>
-  <div>
-    <img src="assets/san_francisco_golden_gate_bridge.jpg" alt="San Francisco - Attraction">
-  </div>
-  <div>
-    <h2>Attraction</h2>
-    <p>
-        The Golden Gate Bridge's vaulting, orange arches amidst the rocky seascape of the San Francisco Bay have made it one of the West Coast's most enduring symbols and the city's most popular tourist attraction. The bridge's name, "Golden Gate," actually refers to the body of water it spans (the Golden Gate Strait that connects the Pacific Ocean with the San Francisco Bay), and was built to make travel between San Francisco and Marin County an easier feat.
+
+  <div class="info">
+    <div>
+      <img src="assets/san_francisco_geo.jpg" alt="San Francisco Geography">
+    </div>
+    <div class="para">
+      <h2>Geography & Demographics</h2>
+      <p>
+        San Francisco is located on the West Coast of the United States at the north end of the San Francisco Peninsula and includes significant stretches of the Pacific Ocean and San Francisco Bay within its boundaries. Several picturesque islands - Alcatraz, Treasure Island and the adjacent Yerba Buena Island, and small portions of Alameda Island, Red Rock Island, and Angel Island - are part of the city. Also included are the uninhabited Farallon Islands, 27 miles (43 km) offshore in the Pacific Ocean. The mainland within the city limits roughly forms a "seven-by-seven-mile square", a common local colloquialism referring to the city's shape, though its total area, including water, is nearly 232 square miles (600 km2).
       </p>
 
       <p>
-        There are plenty of great spots to capture a snap of the majestic bridge. But if you want a truly postcard-worthy shot, head to the Golden Gate Bridge Vista Point, situated high on a hill overlooking San Francisco. If you have extra time, make sure to explore the Golden Gate National Recreation Area. The actual span of the Golden Gate National Recreation Area encompasses multiple places in San Mateo (south of San Francisco), San Francisco and Marin Counties (Alcatraz and Muir Woods included), but notable parts of this recreation area can be found just a stroll away from the Golden Gate. From the bridge, travelers will find some scenic, bayside trails, some of which lead to secluded beaches, including Kirby Cove and Black Sands Beach. If you really want a trek, journey to the Point Bonita Lighthouse for sweeping views of the bay, found at the very tip of the Golden Gate Strait.
+        With a 2020 population of 883,255, it is the 4th largest city in California and the 17th largest city in the United States. San Francisco is currently growing at a rate of 0.10% annually and its population has increased by 9.69% since the most recent census, which recorded a population of 805,235 in 2010. San Francisco reached it's highest population of 883,255 in 2021. Spanning over 232 miles, San Francisco has a population density of 18,832 people per square mile.
       </p>
+    </div>
+    <div>
+      <a href="http://jaddy.digitalscholar.rochester.edu/dms290/assignment1/index.html">Learn more about San Francisco</a>
+    </div>
   </div>
-</div>
+
+  <div class="info">
+    <div>
+      <img src="assets/san_francisco_fog.jpg">
+    </div>
+    <div class="para">
+      <h2>Climate</h2>
+         <p>
+          San Francisco has a warm-summer Mediterranean climate characteristic of California's coast, with moist mild winters and dry summers. San Francisco's weather is strongly influenced by the cool currents of the Pacific Ocean on the west side of the city, and the water of San Francisco Bay to the north and east. This moderates temperature swings and produces a remarkably mild year-round climate with little seasonal temperature variation.
+        </p>
+
+        <p>
+          Among major U.S. cities, San Francisco has the coolest daily mean, maximum, and minimum temperatures for June, July, and August. During the summer, rising hot air in California's interior valleys creates a low pressure area that draws winds from the North Pacific High through the Golden Gate, which creates the city's characteristic cool winds and fog. The fog is less pronounced in eastern neighborhoods and during the late summer and early fall. As a result, the year's warmest month, on average, is September, and on average, October is warmer than July, especially in daytime.
+        </p>
+    </div>
+  </div>
+
+  <div class="info">
+    <div>
+      <img src="assets/san_francisco_1846.jpg" alt="San Francisco - History">
+    </div>
+    <div class="para">
+      <h2>History</h2>
+      <p>
+          The earliest archaeological evidence of human habitation of the territory of the city of San Francisco dates to 3000 BC. The Yelamu group of the Ohlone people resided in a few small villages when an overland Spanish exploration party, led by Don Gaspar de Portolá, arrived on November 2, 1769, the first documented European visit to San Francisco Bay.
+        </p>
+        <p>
+          The first maritime presence occurred on August 5, 1775, when San Carlos - commanded by Juan Manuel de Ayala - became the first ship to anchor in the bay. The following year, on March 28, 1776, the Spanish established the Presidio of San Francisco, followed by a mission, Mission San Francisco de Así­s (Mission Dolores), established by the Spanish explorer Juan Bautista de Anza.
+        </p>
+    </div>
+    <div class="more">
+        <a href="http://jaddy.digitalscholar.rochester.edu/dms290/assignment1/index.html">Learn more about San Francisco</a>
+    </div>
+  </div>
       
-<div>
-  <div>
-    <img src="assets/san_francisco_burritos.jpg" alt="San Francisco - Cuisine">
+  <div class="info">
+    <div>
+      <img src="assets/san_francisco_golden_gate_bridge.jpg" alt="San Francisco - Attraction">
+    </div>
+    <div class="para">
+      <h2>Attraction</h2>
+      <p>
+          The Golden Gate Bridge's vaulting, orange arches amidst the rocky seascape of the San Francisco Bay have made it one of the West Coast's most enduring symbols and the city's most popular tourist attraction. The bridge's name, "Golden Gate," actually refers to the body of water it spans (the Golden Gate Strait that connects the Pacific Ocean with the San Francisco Bay), and was built to make travel between San Francisco and Marin County an easier feat.
+        </p>
+
+        <p>
+          There are plenty of great spots to capture a snap of the majestic bridge. But if you want a truly postcard-worthy shot, head to the Golden Gate Bridge Vista Point, situated high on a hill overlooking San Francisco. If you have extra time, make sure to explore the Golden Gate National Recreation Area. The actual span of the Golden Gate National Recreation Area encompasses multiple places in San Mateo (south of San Francisco), San Francisco and Marin Counties (Alcatraz and Muir Woods included), but notable parts of this recreation area can be found just a stroll away from the Golden Gate. From the bridge, travelers will find some scenic, bayside trails, some of which lead to secluded beaches, including Kirby Cove and Black Sands Beach. If you really want a trek, journey to the Point Bonita Lighthouse for sweeping views of the bay, found at the very tip of the Golden Gate Strait.
+        </p>
+    </div>
   </div>
-  <div>
-    <h2>Cuisine</h2>
-    <h3>Mission-Style Burritos</h3>
+        
+  <div class="info">
+    <div>
+      <img src="assets/san_francisco_burritos.jpg" alt="San Francisco - Cuisine">
+    </div>
+    <div class="para">
+      <h2>Cuisine</h2>
+      <h3>Mission-Style Burritos</h3>
 
-    <p>
-      The Mission-style burrito — or San Francisco burrito — was created in the 1960s in The City’s Mission District. There are two theories as to when and where the first one was served: September 26, 1961 at El Faro, or at September 29, 1969 at Taqueria La Cumbre. Regardless of who was its originator, the Mission-style burrito has become an indispensable native food for San Franciscans.
-    </p>
+      <p>
+        The Mission-style burrito — or San Francisco burrito — was created in the 1960s in The City’s Mission District. There are two theories as to when and where the first one was served: September 26, 1961 at El Faro, or at September 29, 1969 at Taqueria La Cumbre. Regardless of who was its originator, the Mission-style burrito has become an indispensable native food for San Franciscans.
+      </p>
 
-    <p>
-      What sets it apart from the original meat, rice, and beans variety, is that this monster gets super-sized with the addition of extra rice, vegetables, sour cream, and cheese (in a super burrito), and wrapped in a steamed tortilla to keep all the goodness in. Besides El Faro and La Cumbre, another popular burrito institution one should visit to get the traditional Mission style burrito experience is La Taqueria.
-    </p>
+      <p>
+        What sets it apart from the original meat, rice, and beans variety, is that this monster gets super-sized with the addition of extra rice, vegetables, sour cream, and cheese (in a super burrito), and wrapped in a steamed tortilla to keep all the goodness in. Besides El Faro and La Cumbre, another popular burrito institution one should visit to get the traditional Mission style burrito experience is La Taqueria.
+      </p>
+    </div>
   </div>
-</div>
-<!-- Footer -->
-<footer>
+  
+  <footer>
 
-  <h2>Sources</h2>
-  <ul>
-    <li><a href="http://bmart13.digitalscholar.rochester.edu/assignment03/index.html" target="_blank">DMS 290 - Assignment 03 - Group San Francisco</a></li>
-    <li><a href="https://en.wikipedia.org/wiki/San_Francisco" target="_blank">San Francisco</a></li>
-    <li><a href="https://en.wikipedia.org/wiki/San_Francisco#Climate" target="_blank">San Francisco Climate</a></li>
-    <li><a href="https://worldpopulationreview.com/us-cities/san-francisco-ca-population" target="_blank">San Francisco Population</a></li>
-    <li><a href="https://travel.usnews.com/San_Francisco_CA/Things_To_Do/#:~:text=Best%20Things%20To%20Do%20in%20San%20Francisco%201,%26%20Water%20Tours.%20...%204%20Unique%20Experiences.%20" target="_blank">Things to do in San Francisco</a></li>
-    <li><a href="https://www.bayarea.com/eat/food-created-san-francisco" target="_blank">Food created in San Francisco</a></li>
-    <li><a href="https://en.wikipedia.org/wiki/How_Weird_Street_Faire" target="_blank">San Francisco Street Faire</a></li>
-  </ul>
-</footer>
+    <h2>Sources</h2>
+    <ul class="flowing-boxes">
+      <li><a href="http://bmart13.digitalscholar.rochester.edu/assignment03/index.html" target="_blank">DMS 290 - Assignment 03 - Group San Francisco</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/San_Francisco" target="_blank">San Francisco</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/San_Francisco#Climate" target="_blank">San Francisco Climate</a></li>
+      <li><a href="https://worldpopulationreview.com/us-cities/san-francisco-ca-population" target="_blank">San Francisco Population</a></li>
+      <li><a href="https://travel.usnews.com/San_Francisco_CA/Things_To_Do/#:~:text=Best%20Things%20To%20Do%20in%20San%20Francisco%201,%26%20Water%20Tours.%20...%204%20Unique%20Experiences.%20" target="_blank">Things to do in San Francisco</a></li>
+      <li><a href="https://www.bayarea.com/eat/food-created-san-francisco" target="_blank">Food created in San Francisco</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/How_Weird_Street_Faire" target="_blank">San Francisco Street Faire</a></li>
+    </ul>
+  </footer>
 
-<?php include "includes/html-bottom.php"; ?>
+  <?php include "includes/html-bottom.php"; ?>
 
 </body>
 </html>
